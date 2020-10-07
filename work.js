43566cbe3b9b3dbb6db5980317c14cfc546f0334
Osamah <OsDroidi@gmail.com>
2020-10-07 11:15:42 +0800

    first commit

---
+++ work.js
@@ -0,0 +1,83 @@
+
+        < !--gallery system-- >
+        < !-- ! https://codepen.io/peterbenoit/pen/XzVQZw
+
+https://getbootstrap.com/docs/4.2/components/modal/ -->
+
+<div aria-hidden="true" aria-labelledby="myModalLabel" class="modal fade" id="modalIMG" role="dialog"
+    tabindex="-1">
+    <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+
+            <div class="modal-body mb-1 p-1">
+                <!-- <div class="modal-body mb-1 p-1"> -->
+
+
+                        <!-- ! Don't forget to add the class because it's white by deafult -->
+                        <img class="center" src="Image/soon.jpg" alt="" style="width:100%">
+
+                        <br>
+                            <br>
+
+
+
+
+
+                                <br>
+
+                                    <h1 class="lorembig">Lorem ipsum</h1>
+
+                                    <p class="loremsmall">Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi nulla
+                                    fugiat ratione quas quae eaque sequi officia, natus labore reprehenderit explicabo, a iusto
+                            pariatur magnam quibusdam nobis repudiandae rem eum.</p>
+
+
+                                    <p class="loremsmall">Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi nulla
+                                    fugiat ratione quas quae eaque sequi officia, natus labore reprehenderit explicabo, a iusto
+                            pariatur magnam quibusdam nobis repudiandae rem eum.</p>
+
+
+
+                                    <p class="loremsmall">Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi nulla
+                                    fugiat ratione quas quae eaque sequi officia, natus labore reprehenderit explicabo, a iusto
+                            pariatur magnam quibusdam nobis repudiandae rem eum.</p>
+
+
+                                    <p class="loremsmall">Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi nulla
+                                    fugiat ratione quas quae eaque sequi officia, natus labore reprehenderit explicabo, a iusto
+                            pariatur magnam quibusdam nobis repudiandae rem eum.</p>
+
+
+
+
+
+
+
+                                    <br>
+                                        <div class="d-flex justify-content-center">
+                                            <div class="col-md-4 col-lg-5">
+                                                <button class="btn btn-primary btn-block  btn-lg">Order</button>
+
+                                            </div>
+                                        </div>
+                                        <br>
+
+
+                                            <br>
+                                                <br>
+
+                                                    <div class="modal-footer">
+
+
+
+                                                        <button type="button" class="btn btn-secondary btn-lg" data-dismiss="modal">Close</button>
+
+                                                    </div>
+
+                                                    <br>
+
+                    </div>
+
+                </div>
+            </div>
+        </div>
